490abb7eb8b59388a8566fb8579a0ab280b76bc9
Javran Cheng <Javran.c@gmail.com>
2014-09-28 00:56:22 -0400

    test identifer parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,5 @@
-import Language.Scheme.Krampoj.Parser
-
-import Text.Parsec
-
-idTestStr :: String
-idTestStr = "lambda q list->vector soup \
-            \+ V17a <=? a34kTMNs \
-            \the-word-recursion-has-many-meanings"
+module Main
+where
 
 main :: IO ()
-main = putStrLn idTestStr
-    >> print result
-    where
-        result = parse (identifier `sepBy1` space) "" idTestStr
+main = print "placeholder"
---
+++ tests/Test.hs
@@ -0,0 +1,35 @@
+import Test.HUnit
+
+import Language.Scheme.Krampoj.Parser
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Control.Applicative
+import System.Exit
+import Control.Monad
+
+parseAllMaybe :: Parser a -> String -> Maybe a
+parseAllMaybe p x = case parse (p <* eof) "" x of
+                      Left _ -> Nothing
+                      Right v -> Just v
+
+runTestFailIm :: Test -> IO ()
+runTestFailIm t = do
+    cs <- runTestTT t
+    print cs
+    when (errors cs > 0 || failures cs > 0)
+         exitFailure
+
+testIdentifier :: Test
+testIdentifier =
+    "identifier" ~: TestList $ shouldSucceed ++ shouldFail
+    where
+        validIdentifiers = words "lambda q list->vector soup \
+                                 \+ V17a <=? a34kTMNs \
+                                 \the-word-recursion-has-many-meanings"
+
+        shouldSucceed = map ((~=?) <$> Just <*> (parseAllMaybe identifier))
+                            validIdentifiers
+        shouldFail = []
+
+main :: IO ()
+main = runTestFailIm testIdentifier
