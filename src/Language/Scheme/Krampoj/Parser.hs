e4bf011379238ab601fa0d90eb70a2460c67f9d5
Javran Cheng <Javran.c@gmail.com>
2014-09-28 01:35:22 -0400

    num related parsers

--- src/Language/Scheme/Krampoj/Parser.hs
+++ src/Language/Scheme/Krampoj/Parser.hs
@@ -8,6 +8,11 @@ import Data.Char
 import Data.Ix
 import Data.Function
 
+data R = R2
+       | R8
+       | R10
+       | R16
+
 charBetween :: Char -> Char -> Parser Char
 charBetween = curry (satisfy . inRange)
 
@@ -47,3 +52,39 @@ identifier :: Parser String
 identifier =  ((:) <$> initial <*> many subsequent)
           <|> peculiarIdentifier
           <?> "identifier"
+
+digitR :: R -> Parser Char
+digitR R2  = charBetween '0' '1'
+digitR R8  = charBetween '0' '7'
+digitR R10 = digit
+digitR R16 =  digitR R10
+          <|> choice (map ciChar ['a' .. 'f'])
+
+-- TODO: case insensitive string
+radixR :: R -> Parser String
+radixR R2 = string "#b"
+radixR R8 = string "#o"
+radixR R10 = string "#d" <|> empty
+radixR R16 = string "#x"
+
+exactness :: Parser String
+exactness = (char '#' >> ( (char 'i' >> return "#i")
+                       <|> (char 'e' >> return "#e")))
+         <|> empty
+
+sign :: Parser Char
+sign = char '+' <|> char '-' <|> empty
+
+exponentMarker :: Parser Char
+exponentMarker = choice (map ciChar "esfdl")
+
+suffix :: Parser String
+suffix = suffix' <|> empty
+    where
+        -- suffix' = <exponent marker> <sign> <digit10>+
+        suffix' = (:) <$> exponentMarker
+                      <*> ( (:) <$> sign <*> many1 (digitR R10) )
+
+prefix :: R -> Parser String
+prefix r = try ( (++) <$> radixR r <*> exactness)
+           <|> ( (++) <$> exactness <*> radixR r)
